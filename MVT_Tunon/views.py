7f81b0199217a9b71c6c446b6cc548dab435c45b
Federico Tuñon Alves <tunonalves@gmail.com>
2022-05-01 18:42:18 -0300

    varios

--- MVT_Tunon/views.py
+++ MVT_Tunon/views.py
@@ -4,11 +4,11 @@ from .models import family
 from django.template import loader
 
 # Create your views here.
-def familiares(self):
+def familiares(request):
     lista = [family.name,family.lastname,family.parent,family.dni]
     dicc2 = {'lista':lista}
     #diccionario = {'Nombre':family.name,'Apellido':family.lastname,'Parentesco':family.parent,'DNI':family.dni}
     plantilla = loader.get_template("familiares.html")
     documento = plantilla.render(dicc2)
     return HttpResponse(documento)
-     
+
